1194e54e90fe0ca65ff6e3ba577da42f255ffc87
Othman Madjoudj <athmanem@gmail.com>
2019-12-01 13:32:35 +0100

    - Use custom build image based on bionic - Add docker build script - Add xdg desktop file for menu/app overview - Add dynamips and ubridge in the snap

--- docker-build-snap.sh
+++ docker-build-snap.sh
@@ -1,3 +1,6 @@
-export DOCKER=snapcore/snapcraft:stable
-docker pull $DOCKER
+#Docker image is core16 ie. Xenial only
+#export DOCKER=snapcore/snapcraft:stable
+#docker pull $DOCKER
+export DOCKER='gns3.snap:latest'
+docker build -t $DOCKER .
 docker run -it -v "$PWD:$PWD" -w "$PWD" $DOCKER /bin/sh -c 'apt update && snapcraft clean && snapcraft'
