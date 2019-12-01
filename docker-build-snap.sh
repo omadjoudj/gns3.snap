bc1194b3c36f64a9b653648443222c82921dea01
Othman Madjoudj <athmanem@gmail.com>
2019-12-01 13:51:05 +0100

    Switch to core18 and fix docker image var

--- docker-build-snap.sh
+++ docker-build-snap.sh
@@ -1,6 +1,6 @@
 #Docker image is core16 ie. Xenial only
-#export DOCKER=snapcore/snapcraft:stable
-#docker pull $DOCKER
-export DOCKER='gns3.snap:latest'
-docker build -t $DOCKER .
-docker run -it -v "$PWD:$PWD" -w "$PWD" $DOCKER /bin/sh -c 'apt update && snapcraft clean && snapcraft'
+#export DOCKER_IMG=snapcore/snapcraft:stable
+#docker pull $DOCKER_IMG
+export DOCKER_IMG='gns3.snap:latest'
+docker build -t $DOCKER_IMG .
+docker run -it -v "$PWD:$PWD" -w "$PWD" $DOCKER_IMG /bin/sh -c 'apt update && snapcraft clean && snapcraft'
